3abd96b5f6cdabc6eb4e7324b2d89bbdc68f6e9a
Gopi Miyani <gopimiyani@gmail.com>
2019-04-27 21:43:52 -0400

    Front End Added

--- mycloudapp/client/src/App.js
+++ mycloudapp/client/src/App.js
@@ -29,18 +29,31 @@
 
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
+import DefaultLayout from './containers/DefaultLayout';
 import Home from './pages/Home';
+import Login from './views/Pages/Login';
+import Register from './views/Pages/Register';
+import Page404 from './views/Pages/Page404';
+import Page500 from'./views/Pages/Page500';
 import List from './pages/List';
+
+// import { renderRoutes } from 'react-router-config';
+import './App.scss';
+
+const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
+
 
 class App extends Component {
   render() {
     const App = () => (
       <div>
         <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/list' component={List}/>
-          <Route path="*" component={() => <h1>404: Not Found</h1>} />
+              <Route path='/list' component={List}/>
+              <Route  path="/login" component = {Login} />
+              <Route  path="/register" component = {Register}/>
+              <Route  path="/404" component = {Page404}/>
+              <Route  path="/500" component = {Page500}/>
+              <Route path="/" component = {DefaultLayout}/>
         </Switch>
       </div>
     )
---
+++ mycloudapp/client/src/views/Pages/Register/Register.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Button, Card, CardBody, CardFooter, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import { Link, NavLink } from 'react-router-dom';
+class Register extends Component {
+  render() {
+    return (
+      <div className="app flex-row align-items-center">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="9" lg="7" xl="6">
+              <Card className="mx-4">
+                <CardBody className="p-4">
+                  <Form>
+                    <h1>Register</h1>
+                    <p className="text-muted">Create your account</p>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-user"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="Username" autoComplete="username" />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>@</InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="Email" autoComplete="email" />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-lock"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="password" placeholder="Password" autoComplete="new-password" />
+                    </InputGroup>
+                    <InputGroup className="mb-4">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-lock"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="password" placeholder="Repeat password" autoComplete="new-password" />
+                    </InputGroup>
+                 
+                    <Button color="success" className="px-3">Create Account</Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <NavLink to="/login">
+                    <Button color="dark" className="px-5">Back</Button>
+                    </NavLink>
+         
+           
+                  </Form>
+                </CardBody>
+                <CardFooter className="p-4">
+                  {/* <Row>
+                    <Col xs="12" sm="6">
+                      <Button className="btn-facebook mb-1" block><span>facebook</span></Button>
+                    </Col>
+                    <Col xs="12" sm="6">
+                      <Button className="btn-twitter mb-1" block><span>twitter</span></Button>
+                    </Col>
+                  </Row> */}
+                
+                       
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Register;
